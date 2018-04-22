7bc377f73380123a2b8a40a6b643ac05fe562759
Jason Zhuang <zhuangdx@gmail.com>
2018-04-22 22:12:38 +0800

    chapter 1.1

---
+++ ComputerInventory/Models/MachineType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace ComputerInventory.Models
+{
+    public partial class MachineType
+    {
+        public MachineType()
+        {
+            Machine = new HashSet<Machine>();
+        }
+        public int MachineTypeId { get; set; }
+        public string Description { get; set; }
+        public ICollection<Machine> Machine { get; set; }
+    }
+}
---
+++ ComputerInventory/Models/OperatingSys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace ComputerInventory.Models
+{
+    public partial class OperatingSys
+    {
+        public OperatingSys()
+        {
+            Machine = new HashSet<Machine>();
+        }
+        public int OperatingSysId { get; set; }
+        public string Name { get; set; }
+        public bool StillSupported { get; set; }
+        public ICollection<Machine> Machine { get; set; }
+    }
+}
